9d61f2d35075d053322fe5ef57aba6b4cca084c2
Rafał Sienkiewicz <46170208+rafalsienkiewicz@users.noreply.github.com>
2022-05-12 09:20:52 +0200

    fixes

--- .emacs.d/init.el
+++ .emacs.d/init.el
@@ -17,81 +17,129 @@
 (when (not package-archive-contents)
   (package-refresh-contents))
 
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
 
-;; Installs packages
-;;
-;; myPackages contains a list of package names
-(defvar myPackages
-  '(better-defaults                 ;; Set up some better Emacs defaults
-    elpy
-    flycheck
-    py-autopep8
-    blacken
-    magit
-    ein
-    material-theme                  ;; Theme
-    evil
-    jdee
-    )
-  )
+(require 'use-package)
+(setq use-package-always-ensure t)
 
-;; Enable Evil
-(setq evil-disable-insert-state-bindings t)
-(require 'evil)
-(evil-mode 1)
-;; Scans the list in myPackages
-;; If the package listed is not already installed, install it
-(mapc #'(lambda (package)
-          (unless (package-installed-p package)
-            (package-install package)))
-      myPackages)
+;; Thanks, but no thanks
+(setq inhibit-startup-message t)
 
+(scroll-bar-mode -1)        ; Disable visible scrollbar
+(tool-bar-mode -1)          ; Disable the toolbar
+(tooltip-mode -1)           ; Disable tooltips
+(set-fringe-mode 10)        ; Give some breathing room
+(menu-bar-mode -1)            ; Disable the menu bar
+;; Set up the visible bell
+(setq visible-bell t)
 
-;; ===================================
-;; Basic Customization
-;; ===================================
-(setq inhibit-startup-message t)    ;; Hide the startup message
-(load-theme 'material t)            ;; Load theme
-(global-linum-mode t)               ;; Enable line numbers globally
+;; Set the font face based on platform
+(set-face-attribute 'default nil :font "Fira Mono" :height 120)
 
+(load-theme 'wombat)
 
-;; User-Defined init.el ends here
-(custom-set-variables
- ;; custom-set-variables was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- '(package-selected-packages (quote (seti-theme material-theme better-defaults))))
-(custom-set-faces
- ;; custom-set-faces was added by Custom.
- ;; If you edit it by hand, you could mess it up, so be careful.
- ;; Your init file should contain only one such instance.
- ;; If there is more than one, they won't work right.
- )
+(global-set-key (kbd "<escape>") 'keyboard-escape-quit)
 
-;; ====================================
-;; Development Setup
-;; ====================================
-;; Enable elpy
-(set-language-environment "UTF-8")
-(elpy-enable)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-f" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
 
-;; Use IPython for REPL
-;;(setq python-shell-interpreter "jupyter"
-;;     python-shell-interpreter-args "console --simple-prompt"
-;;     python-shell-prompt-detect-failure-warning nil)
+(use-package counsel
+  :after ivy
+  :bind (("M-x" . counsel-M-x)
+         ("C-x b" . counsel-ibuffer)
+         ("C-x C-f" . counsel-find-file)
+         ("C-M-j" . counsel-switch-buffer)
+         ("C-M-l" . counsel-imenu)
+         :map minibuffer-local-map
+         ("C-r" . 'counsel-minibuffer-history))
+  :custom
+  (counsel-linux-app-format-function #'counsel-linux-app-format-function-name-only)
+  :config
+  (setq ivy-initial-inputs-alist nil))
 
-;;(add-to-list 'python-shell-completion-native-disabled-interpreters
-;;             "jupyter")
+(column-number-mode)
+(global-display-line-numbers-mode t)
 
-;; Enable Flycheck
-(when (require 'flycheck nil t)
-  (setq elpy-modules (delq 'elpy-module-flymake elpy-modules))
-  (add-hook 'elpy-mode-hook 'flycheck-mode))
+;; Disable line numbers for some modes
+(dolist (mode '(org-mode-hook
+                erc-mode-hook
+                term-mode-hook
+		shell-mode-hook
+		eshell-mode-hook
+                vterm-mode-hook
+                neotree-mode-hook
+                telega-chat-mode-hook
+                telega-root-mode-hook
+                telega-webpage-mode-hook
+                dashboard-mode-hook))
 
-;; Enable autopep8
-(require 'py-autopep8)
-(add-hook 'elpy-mode-hook 'py-autopep8-enable-on-save)
+(add-hook mode (lambda () (display-line-numbers-mode 0))))
 
+(use-package rainbow-delimiters
+  :hook (prog-mode . rainbow-delimiters-mode))
 
-;; User-Defined init.el ends here 
+(use-package all-the-icons)
+;;(use-package doom-modeline
+;;  :ensure t
+;;  :init (doom-modeline-mode 1))
+
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 0.3))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+(use-package general)
+
+(use-package evil
+  :init
+  (setq evil-want-integration t)
+  (setq evil-want-keybinding nil)
+  (setq evil-want-C-u-scroll t)
+  (setq evil-want-C-i-jump nil)
+  (setq evil-respect-visual-line-mode t)
+  :config
+  (evil-mode 1)
+  (define-key evil-insert-state-map (kbd "C-g") 'evil-normal-state)
+  ;;(define-key evil-insert-state-map (kbd "C-h") 'evil-delete-backward-char-and-join)
+
+  ;; Use visual line motions even outside of visual-line-mode buffers
+  (evil-global-set-key 'motion "j" 'evil-next-visual-line)
+  (evil-global-set-key 'motion "k" 'evil-previous-visual-line)
+
+  ;; Disable arrow keys in normal and visual modes
+
+  (evil-set-initial-state 'messages-buffer-mode 'normal)
+  (evil-set-initial-state 'dashboard-mode 'normal))
+
+(use-package projectile
+  :diminish projectile-mode
+  :config (projectile-mode)
+  :bind-keymap
+  ("C-c p" . projectile-command-map)
+  :init
+  (when (file-directory-p "~/Projects/Code")
+    (setq projectile-project-search-path '("~/Projects/Code")))
+  (setq projectile-switch-project-action #'projectile-dired))
+
